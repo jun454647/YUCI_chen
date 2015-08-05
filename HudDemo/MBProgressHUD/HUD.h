fa4f9007e7c5be1c5ec3988cebd69233eae4b06b
chen <920030752@qq.com>
2015-08-05 11:46:48 +0800

    no message

---
+++ HudDemo/MBProgressHUD/HUD.h
@@ -0,0 +1,13 @@
+//
+//  HUD.h
+//  HudDemo
+//
+//  Created by chen on 15/7/27.
+//  Copyright (c) 2015年 chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HUD : NSObject
+
+@end
---
+++ HudDemo/MBProgressHUD/HUD.m
@@ -0,0 +1,13 @@
+//
+//  HUD.m
+//  HudDemo
+//
+//  Created by chen on 15/7/27.
+//  Copyright (c) 2015年 chen. All rights reserved.
+//
+
+#import "HUD.h"
+
+@implementation HUD
+
+@end
